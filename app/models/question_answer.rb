fc308458c7fc6bcefc03c0886a02a6b4187aa97f
Rob Nichols <rob@undervale.co.uk>
2022-01-24 16:31:16 +0000

    Make join tables act as list

--- app/models/question_answer.rb
+++ app/models/question_answer.rb
@@ -1,4 +1,6 @@
 class QuestionAnswer < ApplicationRecord
   belongs_to :answer
   belongs_to :question
+
+  acts_as_list scope: :question
 end
--- app/models/questionnaire_question.rb
+++ app/models/questionnaire_question.rb
@@ -1,4 +1,6 @@
 class QuestionnaireQuestion < ApplicationRecord
   belongs_to :questionnaire
   belongs_to :question
+
+  acts_as_list scope: :questionnaire
 end
